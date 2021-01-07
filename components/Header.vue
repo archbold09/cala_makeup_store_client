9c5658f2826263ccde8da91a40611c02fd80b2dc
angel_archbold <angelarchbold09@gmail.com>
2021-01-06 21:13:10 -0500

    header end

--- components/Header.vue
+++ components/Header.vue
@@ -1,61 +1,53 @@
 <template>
   <div>
-    <v-app-bar
-      dense
-      dark
-      app
-    >
-    <v-btn icon @click.stop="changeStateSidebar">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    </v-btn>
+    <v-app-bar dense flat>
+      <v-btn icon>
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-whatsapp</v-icon>
+      </v-btn>
 
-      <v-toolbar-title> asdasd </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span>
+        <v-icon left>mdi-phone</v-icon>
+        Llamanos:
+        <v-chip outlined link>+57 3205625795</v-chip>
+      </span>
+    </v-app-bar>
+    <v-app-bar dark>
+      <v-btn icon @click.stop="changeStateSidebar">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </v-btn>
+
+      <v-toolbar-title class="font-weight-black">
+        CALA <span class="font-weight-light">MAKEUP STORE</span>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>INICIO</v-tab>
+          <v-tab>PRODUCTOS</v-tab>
+          <v-tab>NOSOTROS</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
   </div>
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
+import { mapMutations } from 'vuex'
 export default {
   methods: {
-    ...mapMutations([
-      'changeStateSidebar',
-    ])
+    ...mapMutations(['changeStateSidebar']),
   },
 }
 </script>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -13,15 +13,51 @@
     <v-divider></v-divider>
 
     <v-list dense>
-      <v-list-item v-for="item in items" :key="item.title" link>
+      <v-list-item>
         <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
+          <v-icon>mdi-view-dashboard</v-icon>
         </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
+        <v-list-item-title>Inicio</v-list-item-title>
       </v-list-item>
+
+      <v-list-group :value="true" prepend-icon="mdi-package-variant">
+        <template v-slot:activator>
+          <v-list-item-title>Productos</v-list-item-title>
+        </template>
+
+        <v-list-group :value="true" no-action sub-group >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Piel</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item v-for="([title, icon], i) in admins" :key="i" link>
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group no-action sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Actions</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item v-for="([title, icon], i) in cruds" :key="i" link>
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -34,6 +70,16 @@ export default {
       items: [
         { title: 'Home', icon: 'mdi-view-dashboard' },
         { title: 'About', icon: 'mdi-forum' },
+      ],
+      admins: [
+        ['Cremas', 'mdi-account-multiple-outline'],
+        ['Rubores', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
       ],
     }
   },
